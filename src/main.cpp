4a25c850583b0e8dce6b7de542fa1d637ee23e6c
Jakob Mueller <isko.reisen@gmail.com>
2023-06-30 09:25:21 +0200

    Testprogramm

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,17 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
+  pinMode (14 , OUTPUT) ;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(14, HIGH);
+  delay(1000);
+  digitalWrite(14,LOW);
+  delay(1000);
+
 }
 
 // put function definitions here:
